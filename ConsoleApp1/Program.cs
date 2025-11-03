432a3865f169545d3602dd9a6f01c05eb4e3746d
VISIONMECCA <VISIONMECCA@DESKTOP-RE70DKB>
2025-11-03 16:16:12 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿//bool isTrue = true;
+//char character = 'A';
+//char[] chars = {'i', 's', 'T', 'r', 'u', 'e'};
+//string str = new string(chars); // 형변환 ( 문자 배열 -> 문자열 )
+
+//Console.WriteLine(str + " : " + isTrue);
+
+//Console.ReadKey();
+
+
+// 상수, enum 선언
+
+//const int SUNDAY = 0;
+//const int MONDAY = 1;
+//const int TUESDAY = 2;
+//const int WEDNESDAY = 3;
+//const int THURSDAY = 4;
+//const int FRIDAY = 5;
+//const int SATURDAY = 6;
+
+//SUNDAY = 2; // 오류 발생: 상수는 값을 변경할 수 없습니다.
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Days days = Days.Sunday;
+
+//        //if(days == Days.Sunday)
+//        //{
+//        //    Console.WriteLine("오늘은 일요일입니다.");
+//        //}
+//        //if (days == Days.Monday)
+//        //{
+//        //    Console.WriteLine("오늘은 월요일입니다.");
+//        //} 
+
+//        // 위와 같이 변화되는 문자열이나 숫자 대신 enum을 사용하면 가독성이 좋아짐
+
+//        Console.WriteLine(days);
+//        Console.ReadKey();
+//    }
+//}
+
+
+//enum Days // 가독성 up 열거형 선언
+//{
+//    Sunday,     // 0
+//    Monday,     // 1
+//    Tuesday,    // 2
+//    Wednesday,  // 3
+//    Thursday,   // 4
+//    Friday,     // 5
+//    Saturday    // 6
+//};
+
+
+// 명시적 선언 (Explicit Declaration)
+
+// 암시적 선언 (Implicit Declaration)
+
+
+
+Console.ReadKey();
+
